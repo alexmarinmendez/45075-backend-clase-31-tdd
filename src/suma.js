ea67d81f909bf8a8318fce3b30075f2991842fd3
Alex Marin Mendez <alexmarinmendez@gmail.com>
2023-05-03 18:56:40 -0500

    rfc: because we need better code!

--- src/suma.js
+++ src/suma.js
@@ -1,15 +1,16 @@
 export const suma = (...nums) => {
     if (nums.length === 0) return 0
 
-    let validNumber = true
-    for (let index = 0; index < nums.length; index++) {
-        if (typeof nums[index] !== "number") validNumber = false
-    }
-    if(!validNumber) return null
+    // let validNumber = true
+    // for (let index = 0; index < nums.length; index++) {
+    //     if (typeof nums[index] !== "number") validNumber = false
+    // }
+    // if(!validNumber) return null
+    if (nums.some(num => typeof num !== "number")) return null
 
-    let result = 0
-    for (let index = 0; index < nums.length; index++) {
-        result += nums[index]
-    }
-    return result
+    // let result = 0
+    // for (let index = 0; index < nums.length; index++) {
+    //     result += nums[index]
+    // }
+    return nums.reduce((suma, num) => suma + num)
 }
